7b427d4aa0e58d4529dff59f4b6dce76a4989543
Aristid Breitkreuz <aristid.breitkreuz@gmx.de>
2008-11-07 17:01:12 +0100

    fix broken -z MODE

--- util/lcov.sh
+++ util/lcov.sh
@@ -22,4 +22,9 @@
 # THE SOFTWARE.
 #
 
-lcov ${LCOV_MODE:-"-c"} -b build -d build -o build/coverage.info
+lcov ${LCOV_MODE:="-c"} -b ./build -d ./build -o ./build/coverage.info
+
+if [ "$LCOV_MODE" = "-z" ]
+then
+  find ./build -name '*.gcda' -exec rm {} \+
+fi
