ec712f9c0af660bb55c146e4c68b0d8ea6338d33
Ash Berlin <ash@Tehtys.config>
2008-12-03 22:58:42 +0000

    Plugins/curl: Define static constructor properties

--- plugins/curl/curl_plugin.cpp
+++ plugins/curl/curl_plugin.cpp
@@ -39,6 +39,7 @@ public:
     static char const *full_name() { return "cURL"; }
     typedef boost::mpl::bool_<false> constructible;
     static char const *constructor_name() { return "cURL"; }
+    static void augment_constructor(object &ctor);
   };
 };
 
@@ -48,4 +49,30 @@ extern "C" value flusspferd_load(object container)
 {
   return load_class<curl>(container);
 }
+
+///////////////////////////
+void curl::class_info::augment_constructor(object &ctor)
+{
+  curl_version_info_data* data = curl_version_info(CURLVERSION_NOW);
+
+  if (!data) 
+    // Very unlikely to happen, but deal with it
+    throw exception("Unable to get curl_version_info");
+
+  array protocols = create_array();
+  ctor.define_property("supportedProtocols", protocols, read_only_property );
+
+  size_t i = 0;
+  for(const char* const* p = data->protocols; *p != NULL; p++, i++) {
+    protocols.set_element(i, string( *p ) );
+  }
+  // Make the properties of the array read only too
+  //protocols.seal(false);
+  // Arse. Can't in 1.8
+
+  ctor.define_property("versionHex", (int)data->version_num, read_only_property);
+  ctor.define_property("versionStr", string( (const char*)data->version), read_only_property);
+
 }
+
+} // End of anon namespace
