e5a26c054ca7070371efbb8f2143cc59e21f5bf1
Aristid Breitkreuz <aristid.breitkreuz@gmx.de>
2008-12-06 02:14:53 +0100

    Doc: improve naturaldocs runner script

--- util/natural_docs.sh
+++ util/natural_docs.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
-
 mkdir -p html/docs
-NaturalDocs -p naturaldocs -o HTML html/docs -i src/spidermonkey -i src/io \
+if which NaturalDocs >/dev/null
+then
+	ND=NaturalDocs
+else
+	ND=naturaldocs
+fi
+
+$ND -p naturaldocs -o HTML html/docs -i src/spidermonkey -i src/io \
   -i src/xml -s Default flusspferd $@
