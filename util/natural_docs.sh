4488821fe73e758774561ef4ac9b7eb4037c2a79
Ash Berlin <ash_github@firemirror.com>
2008-12-15 23:12:54 +0000

    Misc: fix up natrual docs invocation so it doesn't have abs paths in the Menu.txt file

--- util/natural_docs.sh
+++ util/natural_docs.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
 mkdir -p html/docs
-NaturalDocs -p naturaldocs -o HTML html/docs -i src/spidermonkey -i src/io \
-  -i src/xml -s Default flusspferd $@
+#perl -pi -MCwd -e 's!\$pwd!@{[cwd]}!g' naturaldocs/Menu.txt
+#tail -n 20 naturaldocs/Menu.txt
+
+NaturalDocs -p naturaldocs -o HTML html/docs \
+  -i . \
+  -xi html \
+  -xi include \
+  -do -s Default flusspferd
+
+#tail -n 20 naturaldocs/Menu.txt
+
+#perl -pi -MCwd -e 's!@{[cwd]}!\$pwd!g' naturaldocs/Menu.txt
