f8aead447fe2ce3c6ddbec5fd73680f4dff17c1c
Aristid Breitkreuz <aristid.breitkreuz@gmx.de>
2008-12-16 17:45:05 +0100

    XML: Context automatically updates the XPath namespace hash table now

--- src/xml/context.cpp
+++ src/xml/context.cpp
@@ -28,6 +28,26 @@ THE SOFTWARE.
 using namespace flusspferd;
 using namespace flusspferd::xml;
 
+namespace {
+
+class namespaces : public native_object_base {
+public:
+  namespaces(object const &o, xmlHashTablePtr table);
+  ~namespaces();
+
+  void property_op(property_mode, value const &, value &);
+
+  struct class_info : flusspferd::class_info {
+    static char const *full_name() { return "XML.Context.Namespaces"; }
+    typedef boost::mpl::bool_<false> constructible;
+  };
+
+private:
+  xmlHashTablePtr table;
+};
+
+}
+
 xml::context::context(object const &obj, call_context &x)
   : native_object_base(obj)
 {
@@ -39,7 +59,12 @@ xml::context::context(object const &obj, call_context &x)
   if (!xpath_ctx)
     throw exception("Could not initialise XPath context");
 
-  object ns = create_object();
+  xpath_ctx->nsHash = xmlHashCreate(1);
+
+  if (!xpath_ctx->nsHash)
+    throw exception("Could not initialise XPath context");
+
+  object ns = create_native_object<namespaces>(object(), xpath_ctx->nsHash);
   ns.set_property("xml", string("http://www.w3.org/XML/1998/namespace"));
   set_property("ns", ns);
 }
@@ -52,4 +77,42 @@ object xml::context::class_info::create_prototype() {
   object proto = create_object();
 
   return proto;
+}
+
+namespaces::namespaces(object const &obj, xmlHashTablePtr table)
+  : native_object_base(obj), table(table)
+{
+}
+
+namespaces::~namespaces() {}
+
+void namespaces::property_op(
+  property_mode mode, value const &id, value &data)
+{
+  local_root_scope scope;
+  string name_ = id.to_string();
+  xmlChar const *name = (xmlChar const*) name_.c_str();
+
+  xmlChar *txt = 0;
+  if (mode != property_get && mode != property_delete) {
+    data = data.to_string();
+    txt = xmlStrdup((xmlChar const *) data.get_string().c_str());
+  }
+
+  switch (mode) {
+  case property_add:
+    if (xmlHashAddEntry(table, name, txt) != 0)
+      throw exception("Could not add entry to namespace hash table");
+    break;
+  case property_delete:
+    if (xmlHashRemoveEntry(table, name, xmlHashDeallocator(xmlFree)) != 0)
+      throw exception("Could not remove entry from namespace hash table");
+    break;
+  case property_set:
+    if (xmlHashUpdateEntry(table, name, txt, xmlHashDeallocator(xmlFree))!=0)
+      throw exception("Could not update entry in namespace hash table");
+    break;
+  case property_get:
+    break;
+  }
 }
