2117676e179521a2351e8edad7291241927f71e8
Aristid Breitkreuz <aristid.breitkreuz@gmx.de>
2009-03-26 22:38:02 +0100

    doc: add ref docs for blob (C++ side)

--- include/flusspferd/blob.hpp
+++ include/flusspferd/blob.hpp
@@ -29,12 +29,44 @@ THE SOFTWARE.
 
 namespace flusspferd {
 
+/**
+ * Binary data storage class.
+ *
+ * A Blob can store binary data that Javascript cannot otherwise store.
+ *
+ * Wraps a (mutable) std::vector<unsigned char>.
+ *
+ * @see <a href="docs/files/src/spidermonkey/blob-jsdoc.html">Javascript 'Blob'</a>.
+ */
 class blob : public native_object_base {
 public:
+  /**
+   * Javascript constructor.
+   *
+   * @param obj The object to associate with.
+   * @param x The function call_context.
+   */
   blob(object const &obj, call_context &x);
+
+  /**
+   * Data constructor.
+   *
+   * Data will be copied.
+   *
+   * @param obj The object to associate with.
+   * @param data Pointer to the data.
+   * @param length Length of the data.
+   *
+   * @see flusspferd::create_native_object
+   */
   blob(object const &obj, unsigned char const *data, std::size_t length);
+
+  /// Destructor.
   ~blob();
 
+  /**
+   * Javascript class info.
+   */
   struct class_info : flusspferd::class_info {
     static char const *full_name() { return "Blob"; }
 
@@ -45,9 +77,25 @@ public:
     static void augment_constructor(object &);
   };
 
+  /**
+   * Access the data.
+   *
+   * @return Pointer to the data.
+   */
   unsigned char *data() { return &data_[0]; }
+
+  /**
+   * Get size.
+   *
+   * @return The size.
+   */
   std::size_t size() { return data_.size(); }
 
+  /**
+   * Access the data as a std::vector<unsigned char>.
+   *
+   * @return A reference to the data.
+   */
   std::vector<unsigned char> &get() { return data_; }
 
 protected:
