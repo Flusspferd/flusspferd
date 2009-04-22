b6231d2bdf5b580ecaa861b70080be38cf2a3bc3
Aristid Breitkreuz <aristid.breitkreuz@gmx.de>
2009-04-22 13:22:59 +0200

    directories: jsdoc_template => help/jsdoc_template

--- util/jsdocs.sh
+++ util/jsdocs.sh
@@ -3,5 +3,5 @@
 java -cp vendor/jsdoc_toolkit-2.1.0/jsrun.jar \
   JsRun \
   vendor/jsdoc_toolkit-2.1.0/app/run.js \
-  -c=jsdoc_template/config.json \
+  -c=help/jsdoc_template/config.json \
   src/js plugins/ src
