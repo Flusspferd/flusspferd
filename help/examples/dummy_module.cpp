d66baa142a9094d4379a48138d8c07437a521a5f
Aristid Breitkreuz <aristid.breitkreuz@gmx.de>
2009-04-24 18:48:55 +0200

    doc: modify modules tutorial to reflect new reality

--- help/examples/dummy_module.cpp
+++ help/examples/dummy_module.cpp
@@ -2,7 +2,6 @@
 #include <iostream>
 #include <ostream>
 
-extern "C" flusspferd::value flusspferd_load(flusspferd::object container) {
+extern "C" void flusspferd_load(flusspferd::object container) {
     std::cout << "Module loaded!" << std::endl;
-    return flusspferd::string("Here, take this cookie.");
 }
