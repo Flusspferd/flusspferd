c9235676becb77d08498d3c13bec56a3ab359ef9
Aristid Breitkreuz <aristid.breitkreuz@gmx.de>
2009-04-28 18:08:00 +0200

    doc: make tutorial more interesting by collapsing first_program and second_program together

--- help/examples/first_program.cpp
+++ help/examples/first_program.cpp
@@ -1,9 +1,25 @@
 #include <flusspferd.hpp>
+#include <iostream>
+#include <ostream>
 
 int main() {
+    // Create and use a context, needed to be able to use Flusspferd.
     flusspferd::current_context_scope context_scope(
         flusspferd::context::create());
 
-    // Flusspferd is now initialized. Now we can do what we really want to do:
-    // nothing.
+    // Create a new string and protect it from the garbage collector by making
+    // it a root string.
+    flusspferd::root_string str("Hello, World!\n");
+
+    // Demonstrate that str will not be garbage collected, by calling the
+    // garbage collector.
+    // Note that there is no other reason to call the garbage collector here.
+    flusspferd::gc();
+
+    // Set a property on the global object. Effectively, this creates a global
+    // variable "str" with the contents of str.
+    flusspferd::global().set_property("str", str);
+
+    // Print str on std::cout - but first, replace "World" by "Flusspferd".
+    std::cout << flusspferd::evaluate("str.replace('World', 'Flusspferd')");
 }
