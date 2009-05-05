c89c0324ef1a0538379f8c96bf2ee8b68d66aebc
Aristid Breitkreuz <aristid.breitkreuz@gmx.de>
2009-05-05 16:44:46 +0200

    class_macros: add variable property declarator

--- include/flusspferd/class_description.hpp
+++ include/flusspferd/class_description.hpp
@@ -263,6 +263,12 @@ THE SOFTWARE.
     ::flusspferd::read_only_property | ::flusspferd::permanent_property); \
   /* */
 
+#define FLUSSPFERD_CD_PROPERTY__variable(p_cpp_name, p_property_name, p_param) \
+  obj.define_property( \
+    (p_property_name), \
+    (p_param)); \
+  /* */
+
 #define FLUSSPFERD_CD_PROPERTY__none(p_cpp_name, p_property_name, p_param) \
   /* */
 
