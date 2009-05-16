612bc473b3fb471756514f980c000f79684b60fd
Aristid Breitkreuz <aristid.breitkreuz@gmx.de>
2009-05-16 13:24:43 +0200

    copyright: make copyright lines uniform

--- test/js/test-http-headers.js
+++ test/js/test-http-headers.js
@@ -1,6 +1,6 @@
 // vim:ts=2:sw=2:expandtab:autoindent:
 /*
-Copyright (c) 2008 Aristid Breitkreuz, Ash Berlin, Ruediger Sonderfeld
+Copyright (c) 2008, 2009 Aristid Breitkreuz, Ash Berlin, Ruediger Sonderfeld
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
