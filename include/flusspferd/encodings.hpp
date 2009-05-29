83d9be10cd04570726fd14b057f24a01e489bf1e
Ash Berlin <ash_github@firemirror.com>
2009-05-29 23:17:58 +0100

    Stub out encodings module

---
+++ include/flusspferd/encodings.hpp
@@ -0,0 +1,42 @@
+// vim:ts=2:sw=2:expandtab:autoindent:filetype=cpp:
+/*
+Copyright (c) 2009 Flusspferd Team
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+#ifndef FLUSSPFERD_ENCODINGS_HPP
+#define FLUSSPFERD_ENCODINGS_HPP
+
+#include "string.hpp"
+#include "binary.hpp"
+
+namespace flusspferd {
+
+  void load_encodings_module(object container);
+  namespace encodings {
+
+    string convert_to_string(const char* enc, binary const &source);
+    object convert_from_string(const char* enc, string const &source);
+    object convert(const char* fromEnc, const char* toEnc, binary const &source);
+  }
+
+}
+
+#endif
+
---
+++ src/spidermonkey/encodings.cpp
@@ -0,0 +1,62 @@
+// vim:ts=2:sw=2:expandtab:autoindent:filetype=cpp:
+/*
+Copyright (c) 2009 Flusspferd Team
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+#include "flusspferd/binary.hpp"
+#include "flusspferd/encodings.hpp"
+#include "flusspferd/create.hpp"
+
+using namespace flusspferd;
+
+void flusspferd::load_encodings_module(object container) {
+  object exports = container.get_property("exports").to_object();
+
+  // Load the binary module
+  global().call("require", "binary");
+
+  create_native_function(
+    exports,
+    "convertToString", &encodings::convert_to_string);
+
+  create_native_function(
+    exports,
+    "convertFromString", &encodings::convert_from_string);
+
+  //create_native_function( exports, "convert", &encodings::convert);
+}
+
+string encodings::convert_to_string(const char *, binary const &) {
+  return string();
+}
+
+
+object encodings::convert_from_string(const char* enc, string const &source) {
+  size_t n = 0;
+  return create_native_object<byte_string>(object(), (unsigned char*)"", n);
+}
+
+object encodings::convert(const char*fromEnc, const char *toEnc,
+    binary const &source) {
+
+  size_t n = 0;
+  return create_native_object<byte_string>(object(), (unsigned char*)"", n);
+}
