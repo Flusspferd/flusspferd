b888ecd4df3550d0a0578b3eb61e468cb2f2d889
Aristid Breitkreuz <aristid.breitkreuz@gmx.de>
2009-06-06 19:48:35 +0200

    pack.sh: add .zip support

--- pack.sh
+++ pack.sh
@@ -3,7 +3,7 @@ VER=`./flusspferd-version.sh`
 echo $VER > version
 FILES="version `git ls-files|egrep -v '^.git'`"
 PREFIX="/tmp/flusspferd-$VER/"
-rm -r $PREFIX
+rm -r $PREFIX || exit 1
 mkdir $PREFIX
 for file in $FILES
 do
@@ -12,8 +12,15 @@ do
 	mkdir -p $dir 2>/dev/null
 	cp $file $dir/
 done
+
 cd /tmp
+
 tar czf flusspferd-$VER.tar.gz flusspferd-$VER/
 echo $PWD/flusspferd-$VER.tar.gz
+
 tar cjf flusspferd-$VER.tar.bz2 flusspferd-$VER/
 echo $PWD/flusspferd-$VER.tar.bz2
+
+rm -f flusspferd-$VER.zip
+zip -r -q flusspferd-$VER.zip flusspferd-$VER/
+echo $PWD/flusspferd-$VER.zip
