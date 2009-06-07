dda9ba44d41b62b112e24702c5a18707f60f3fcc
Ash Berlin <ash_github@firemirror.com>
2009-06-07 22:56:13 +0100

    test/encodings: Convert to proper a proper test suite

--- test/js/encodings.js
+++ test/js/encodings.js
@@ -1,68 +1,52 @@
-"use strict";
-
-// TODO: Convert these to proper tests
-
 const binary = require('binary');
 const encodings = require('encodings');
+const asserts = require('test').asserts;
 
+// Test data.
 const e_accute_utf8 = binary.ByteString([0xC3,0xA9]);
 const partial_utf8 = binary.ByteString([0x,68,0xC3]);
 const invalid_utf8 = binary.ByteString([0x,68,0xC3,0x6C]);
 
-const e_accute_utf16 = binary.ByteString([0x0,0xe9,0x0]);
+const e_accute_utf16 = binary.ByteString([0x0,0xe9]);
 const partial_utf16 = binary.ByteString([0x,68,0xC3]);
 const invalid_utf16 = binary.ByteString([0x,68,0xC3,0x6C]);
 
 const ligature_oe_iso8859_15 = binary.ByteString([0xBD]);
 
-//print(encodings.convertToString("utf-8", test1));
+if (!this.exports) this.exports = {};
 
-function test_convertSameEnc() {
+exports.test_convertSameEnc = function() {
   var blob = encodings.convert("utf-8", "utf-8", e_accute_utf8);
 
-  print(blob !== e_accute_utf8);
-  print(blob.toSource());
+  asserts.ok(blob !== e_accute_utf8, "different blob returned");
+  asserts.same(blob.toArray(), [195,169]);
   
 }
 
-function test_convertToString() {
+exports.test_convertToString = function() {
   var str = encodings.convertToString("utf-8", e_accute_utf8);
 
-  print(str.toSource());
-  print(typeof str == "string");
-  print(str.length == 1);
-  print (str == "\xE9");
-  
+  asserts.same(str, "\xE9")
 }
-function test_convertToStringUTF16() {
+
+
+exports.test_convertToStringUTF16 = function() {
   var str = encodings.convertToString("utf-16", e_accute_utf16);
 
-  print(str.toSource());
-  print(typeof str == "string");
-  print(str.length == 1);
-  print (str == "\xE9"); // é
-  
+  asserts.same(str, "\xE9") // é
 }
 
-function test_convertToStringISO8859_15() {
+exports.test_convertToStringISO8859_15 = function() {
   var str = encodings.convertToString("iso-8859-15", ligature_oe_iso8859_15);
 
-  print(str.toSource());
-  print(typeof str == "string");
-  print(str.length == 1);
-  print (str == "\u0153"); //œ
+  asserts.same(str, "\u0153"); //œ
 }
 
-function test_convert() {
+exports.test_convert = function() {
   var blob = encodings.convert("utf-8", "UTF-16", e_accute_utf8);
 
-  print(blob !== e_accute_utf8);
-  print(blob.toSource());
-  
+  asserts.same(blob.toArray(), [254,255,0,233]);
 }
 
-//test_convertSameEnc();
-//test_convertToString();
-//test_convertToStringUTF16();
-test_convertToStringISO8859_15();
-//test_convert();
+// if (module.isMain)
+  require('test').runner(exports);
