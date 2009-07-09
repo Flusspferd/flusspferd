a6585611f19145f20d7baa27be485f44409ea960
Aristid Breitkreuz <aristid@weingummi.(none)>
2009-07-09 19:42:06 +0200

    doc: more modules tutorial stuff

--- help/examples/dummy_module.cpp
+++ help/examples/dummy_module.cpp
@@ -2,6 +2,12 @@
 #include <iostream>
 #include <ostream>
 
+void do_nothing_useful() {
+    std::cout << "Yup, this is a function that does nothing useful." << std::endl;
+}
+
 FLUSSPFERD_LOADER_SIMPLE(exports) {
+    flusspferd::create_native_function(exports, "doNothingUseful", &do_nothing_useful);
+    exports.set_property("variable", value(true));
     std::cout << "Module loaded!" << std::endl;
 }
