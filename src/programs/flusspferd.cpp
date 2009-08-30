6127303607bc84c4e0be19c258db8e1210e90d5d
Aristid Breitkreuz <aristid.breitkreuz@gmx.de>
2009-08-31 01:22:16 +0200

    shell: Use new getopt API for parsing interpreter / shell arguments

--- src/programs/flusspferd.cpp
+++ src/programs/flusspferd.cpp
@@ -28,6 +28,7 @@ THE SOFTWARE.
 #include "flusspferd/spidermonkey/object.hpp"
 #include <boost/spirit/home/phoenix/core.hpp>
 #include <boost/spirit/home/phoenix/bind.hpp>
+#include <boost/spirit/home/phoenix/operator.hpp>
 #include <iostream>
 #include <fstream>
 #include <cstring>
@@ -53,11 +54,12 @@ namespace args = phoenix::arg_names;
 
 class flusspferd_repl {
   bool interactive;
+  bool interactive_set;
   bool machine_mode;
   std::istream in;
 
   bool config_loaded;
-  char const *config_file;
+  std::string config_file;
 
   flusspferd::context co;
   flusspferd::current_context_scope scope;
@@ -71,9 +73,16 @@ class flusspferd_repl {
   char ** argv;
 
   enum Type { File, Expression, IncludePath, Module, MainModule };
+
+  std::list<std::pair<std::string, Type> > files;
+
+  flusspferd::object option_spec();
   
   // Returns list of files / expressions to execute
-  std::list<std::pair<std::string, Type> > parse_cmdline();
+  void parse_cmdline();
+  void print_help(bool do_quit = false);
+  void print_version();
+  void add_file(std::string const &path, Type type, bool del_interactive);
   void load_config();
 
   bool getline(std::string &source, const char* prompt = "> ");
@@ -92,6 +101,7 @@ public:
 
 flusspferd_repl::flusspferd_repl(int argc, char **argv)
   : interactive(true),
+    interactive_set(false),
     machine_mode(false),
     //file("typein"),
     in(std::cin.rdbuf()),
@@ -124,7 +134,7 @@ flusspferd_repl::flusspferd_repl(int argc, char **argv)
 }
 
 int flusspferd_repl::run() {
-  std::list<std::pair<std::string, Type> > files = parse_cmdline();
+  parse_cmdline();
 
   if (!config_loaded)
     load_config();
@@ -220,9 +230,8 @@ int flusspferd_repl::run() {
   return exit_code;
 }
 
-void print_help(char const *argv0) {
-  std::cerr << "usage: " << argv0 <<
-" [option] ... [file | -] [arg] ...\n\n"
+void flusspferd_repl::print_help(bool do_quit) {
+  std::cerr << "usage: " << argv[0] << " [option] ... [file | -] [arg] ...\n\n"
 "Options\n"
 "    -h\n"
 "    --help                   Displays this message.\n"
@@ -236,7 +245,8 @@ void print_help(char const *argv0) {
 "    -i\n"
 "    --interactive            Enter interactive mode (after files).\n"
 "\n"
-"    -0                       (Interactive) machine command mode (separator\n"
+"    -0\n"
+"    --machine-mode           (Interactive) machine command mode (separator\n"
 "                             '\\0').\n"
 "\n"
 "    -e <expr>\n"
@@ -245,7 +255,8 @@ void print_help(char const *argv0) {
 "    -f <file>\n"
 "    --file <file>            Run this file before standard script handling.\n"
 "\n"
-"    -I <path>\n              Add include path."
+"    -I <path>\n"
+"    --include-path <path>    Add include path."
 "\n"
 "    -M <module>\n            Load module."
 "\n"
@@ -257,210 +268,189 @@ void print_help(char const *argv0) {
 "    --history-file <file>\n  Sets history file (default: ~/.flusspferd-history)\n"
 "\n"
 "    --                       Stop processing options.\n\n";
+
+  if (do_quit)
+    throw flusspferd::js_quit();
 }
 
-void print_version() {
+void flusspferd_repl::print_version() {
   std::cout << "flusspferd shell version: " << FLUSSPFERD_VERSION << '\n';
   std::cout << "flusspferd library version: " << flusspferd::version() << '\n';
   std::cout.flush();
+  throw flusspferd::js_quit();
+}
+
+void flusspferd_repl::add_file(
+    std::string const &file, Type type, bool del_interactive)
+{
+  if (del_interactive && !interactive_set)
+    interactive = false;
+  files.push_back(std::make_pair(file, type));
 }
 
 void flusspferd_repl::load_config() {
-  flusspferd::execute(config_file);
+  flusspferd::execute(config_file.c_str());
   config_loaded = true;
 
   // Get the prelude and execute it too
   flusspferd::value prelude = co.global().get_property("prelude");
   
   if (!prelude.is_undefined_or_null()) {
-    flusspferd::execute(prelude.to_string().c_str(), flusspferd::global().prototype());
+    flusspferd::execute(
+      prelude.to_string().c_str(),
+      flusspferd::global().prototype());
   }
 }
 
-std::list<std::pair<std::string, flusspferd_repl::Type> >
-flusspferd_repl::parse_cmdline() {
-  bool interactive_set = false;
+flusspferd::object flusspferd_repl::option_spec() {
+  flusspferd::root_object spec(flusspferd::create_object());
 
-  std::list<std::pair<std::string, Type> > files;
+  flusspferd::object help(flusspferd::create_object());
+  spec.set_property("help", help);
+  help.set_property("alias", "h");
+  flusspferd::create_native_function(help, "callback",
+    phoenix::bind(&flusspferd_repl::print_help, this, true));
 
-  int i=1;
+  flusspferd::object version(flusspferd::create_object());
+  spec.set_property("version", version);
+  version.set_property("alias", "v");
+  flusspferd::create_native_function(version, "callback",
+    phoenix::bind(&flusspferd_repl::print_version, this));
 
-  for (i = 1; i < argc; ++i) {
-    if (argv[i][0] == '-') {
-      if (std::strcmp(argv[i], "--") == 0)
-      {
-        i++;
-        break;
-      }
-      else if (std::strcmp(argv[i], "-h") == 0 ||
-          std::strcmp(argv[i], "--help") == 0)
-      {
-        print_help(argv[0]);
-        throw flusspferd::js_quit();
-      }
-      else if (std::strcmp(argv[i], "-v") == 0 ||
-          std::strcmp(argv[i], "--version") == 0)
-      {
-        print_version();
-        throw flusspferd::js_quit();
-      }
-      else if (std::strcmp(argv[i], "-c") == 0 ||
-               std::strcmp(argv[i], "--config") == 0)
-      {
-        ++i;
-        if (i == argc) {
-          print_help(argv[0]);
-          std::string msg = "expected filename after ";
-          msg += argv[i-1];
-          msg += " option\n";
-          throw std::runtime_error(msg);
-        }
-        config_file = argv[i];
-      }
-      else if (std::strcmp(argv[i], "-i") == 0 ||
-               std::strcmp(argv[i], "--interactive") == 0)
-      {
-        interactive = true;
-        interactive_set = true;
-      }
-      else if (std::strcmp(argv[i], "-0") == 0)
-      {
-        interactive = true;
-        interactive_set = true;
-        machine_mode = true;
-      }
-      else if (std::strcmp(argv[i], "-f") == 0 ||
-               std::strcmp(argv[i], "--file") == 0)
-      {
-        ++i;
-        if (i == argc) {
-          print_help(argv[0]);
-          std::string msg = "expected filename after ";
-          msg += argv[i-1];
-          msg += " option\n";
-          throw std::runtime_error(msg);
-        }
-        std::string file = argv[i];
-        if (!interactive_set)
-          interactive = false;
-        files.push_back(std::make_pair(file, File));
-      }
-      else if (std::strcmp(argv[i], "-e") == 0 ||
-               std::strcmp(argv[i], "--expression") == 0)
-      {
-        ++i;
-        if (i == argc) {
-          print_help(argv[0]);
-          std::string msg = "expected expression after ";
-          msg += argv[i-1];
-          msg += " option\n";
-          throw std::runtime_error(msg);
-        }
-        std::string file = argv[i];
-        if (!interactive_set)
-          interactive = false;
-        files.push_back(std::make_pair(file, Expression));
-      }
-      else if (std::strcmp(argv[i], "-I") == 0)
-      {
-        ++i;
-        if (i == argc) {
-          print_help(argv[0]);
-          std::string msg = "expected expression after ";
-          msg += argv[i-1];
-          msg += " option\n";
-          throw std::runtime_error(msg);
-        }
-        std::string path = argv[i];
-        files.push_back(std::make_pair(path, IncludePath));
-      }
-      else if (std::strcmp(argv[i], "-M") == 0)
-      {
-        ++i;
-        if (i == argc) {
-          print_help(argv[0]);
-          std::string msg = "expected expression after ";
-          msg += argv[i-1];
-          msg += " option\n";
-          throw std::runtime_error(msg);
-        }
-        std::string path = argv[i];
-        files.push_back(std::make_pair(path, Module));
-      }
-      else if (std::strcmp(argv[i], "-m") == 0 ||
-               std::strcmp(argv[i], "--main") == 0)
-      {
-        ++i;
-        if (i == argc) {
-          print_help(argv[0]);
-          std::string msg = "expected expression after ";
-          msg += argv[i-1];
-          msg += " option\n";
-          throw std::runtime_error(msg);
-        }
-        std::string path = argv[i];
-        files.push_back(std::make_pair(path, MainModule));
-        if (!interactive_set)
-          interactive = false;
-      }
-      else if (std::strcmp(argv[i], "--no-global-history") == 0)
-      {
-        history_file.clear();
-      }
-      else if (std::strcmp(argv[i], "--history-file") == 0)
-      {
-        ++i;
-        if (i == argc) {
-          print_help(argv[0]);
-          std::string msg = "expected expression after ";
-          msg += argv[i-1];
-          msg += " option\n";
-          throw std::runtime_error(msg);
-        }
-        history_file = argv[i];
-      }
-      else
-      {
-        print_help(argv[0]);
-        throw std::runtime_error(std::string("invalid option: ")+argv[i]+"\n");
-      }
-    }
-    else
-      break; // Not an option, stop looking for one
-  }
+  flusspferd::object config(flusspferd::create_object());
+  spec.set_property("config", config);
+  config.set_property("alias", "c");
+  config.set_property("argument", "required");
+  flusspferd::create_native_function(config, "callback",
+    boost::function<void (flusspferd::value, std::string)>(
+      phoenix::ref(config_file) = args::arg2
+    ));
+
+  flusspferd::object interactive_(flusspferd::create_object());
+  spec.set_property("interactive", interactive_);
+  interactive_.set_property("alias", "i");
+  flusspferd::create_native_function(interactive_, "callback",
+    boost::function<void ()>((
+      phoenix::ref(interactive) = true,
+      phoenix::ref(interactive_set) = true
+    )));
+
+  flusspferd::object machine_mode_(flusspferd::create_object());
+  spec.set_property("machine-mode", machine_mode_);
+  machine_mode_.set_property("alias", "0");
+  flusspferd::create_native_function(interactive_, "callback",
+    boost::function<void ()>((
+      phoenix::ref(machine_mode) = true,
+      phoenix::ref(interactive) = true,
+      phoenix::ref(interactive_set) = true
+    )));
+
+  flusspferd::object file(flusspferd::create_object());
+  spec.set_property("file", file);
+  file.set_property("alias", "f");
+  file.set_property("argument", "required");
+  flusspferd::create_native_function(file, "callback",
+    boost::function<void (flusspferd::value, std::string)>(
+      phoenix::bind(&flusspferd_repl::add_file, this,
+                    args::arg2, File, true)
+    ));
+
+  flusspferd::object expression(flusspferd::create_object());
+  spec.set_property("expression", expression);
+  expression.set_property("alias", "e");
+  expression.set_property("argument", "required");
+  flusspferd::create_native_function(expression, "callback",
+    boost::function<void (flusspferd::value, std::string)>(
+      phoenix::bind(&flusspferd_repl::add_file, this,
+                    args::arg2, Expression, true)
+    ));
+
+  flusspferd::object include_path(flusspferd::create_object());
+  spec.set_property("include-path", include_path);
+  include_path.set_property("alias", "I");
+  include_path.set_property("argument", "required");
+  flusspferd::create_native_function(include_path, "callback",
+    boost::function<void (flusspferd::value, std::string)>(
+      phoenix::bind(&flusspferd_repl::add_file, this,
+                    args::arg2, IncludePath, false)
+    ));
+
+  flusspferd::object module(flusspferd::create_object());
+  spec.set_property("module", module);
+  module.set_property("alias", "M");
+  module.set_property("argument", "required");
+  flusspferd::create_native_function(module, "callback",
+    boost::function<void (flusspferd::value, std::string)>(
+      phoenix::bind(&flusspferd_repl::add_file, this,
+                    args::arg2, Module, false)
+    ));
+
+  flusspferd::object main_module(flusspferd::create_object());
+  spec.set_property("main", main_module);
+  main_module.set_property("alias", "m");
+  main_module.set_property("argument", "required");
+  flusspferd::create_native_function(main_module, "callback",
+    boost::function<void (flusspferd::value, std::string)>(
+      phoenix::bind(&flusspferd_repl::add_file, this,
+                    args::arg2, MainModule, true)
+    ));
+
+  flusspferd::object no_global_history(flusspferd::create_object());
+  spec.set_property("no-global-history", no_global_history);
+  flusspferd::create_native_function(no_global_history, "callback",
+    boost::function<void()>(
+      phoenix::ref(history_file) = std::string()
+    ));
+
+  flusspferd::object history_file_(flusspferd::create_object());
+  spec.set_property("history-file", history_file_);
+  history_file_.set_property("argument", "required");
+  flusspferd::create_native_function(history_file_, "callback",
+    boost::function<void (flusspferd::value, std::string)>(
+      phoenix::ref(history_file) = args::arg2
+    ));
+
+  return spec;
+}
+
+void flusspferd_repl::parse_cmdline() {
+  flusspferd::root_object spec(option_spec());
+
+  flusspferd::array arguments(flusspferd::create_array());
+
+  for (int i = 1; i < argc; ++i)
+    arguments.call("push", std::string(argv[i]));
+
+  flusspferd::root_object results(flusspferd::getopt(spec, arguments));
 
   if (!config_loaded)
     load_config();
 
-  flusspferd::array args =
-    flusspferd::global()
-      .call("require", "system").to_object()
-      .get_property_object("args");
+  arguments = results.get_property_object("_");
 
-  args.set_element(0, flusspferd::value("-"));
+  std::string file("-");
 
-  if (i < argc) {
-    // some cmd line args left.
-    // first one is file
-    // others go into arguments array
-    std::string file = argv[i++];
-    if (file != "-") { // TODO: Maybe check if stdin is actualy connected to a terminal?
+  if (arguments.size() > 0) {
+    file = arguments.call("shift").to_std_string();
+    // TODO: Maybe check if stdin is actualy connected to a terminal?
+    if (file != "-") {
       files.push_back(std::make_pair(file, File));
       if (!interactive_set)
         interactive = false;
-    }
-
-    args.set_element(0, flusspferd::value(file));
-
-    int x = 1;
-    for (; i < argc; ++i) {
-      args.set_element(
-        x++,
-        flusspferd::value(const_cast<char const*>(argv[i])));
+    } else {
+      interactive = true;
     }
   }
 
-  return files;
+  arguments.call("unshift", file);
+
+  flusspferd::object sys =
+    flusspferd::global()
+      .call("require", "system").to_object();
+
+  sys.define_property("args", arguments,
+                      flusspferd::read_only_property |
+                      flusspferd::permanent_property);
 }
 
 namespace {
