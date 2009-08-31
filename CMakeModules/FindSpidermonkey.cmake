3401d355b94578dd6f9bdc6831d87ba3399855ed
Aristid Breitkreuz <aristid.breitkreuz@gmx.de>
2009-08-31 23:17:25 +0200

    build: prefer mozjs over js because modern versions are mozjs

--- CMakeModules/FindSpidermonkey.cmake
+++ CMakeModules/FindSpidermonkey.cmake
@@ -52,13 +52,13 @@ find_path(
 
 find_library(
     SPIDERMONKEY_LIBRARY
-    NAMES js mozjs js32
+    NAMES mozjs js js32
     PATHS "${SPIDERMONKEY_ROOT}/lib"
     NO_DEFAULT_PATH)
 
 find_library(
     SPIDERMONKEY_LIBRARY
-    NAMES js mozjs js32)
+    NAMES mozjs js js32)
 
 set(SPIDERMONKEY_LIBRARIES ${SPIDERMONKEY_LIBRARY})
 
