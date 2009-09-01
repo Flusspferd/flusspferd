3e8e5e33778f70680cdae20e94efc492013830fb
Aristid Breitkreuz <aristid.breitkreuz@gmx.de>
2009-09-01 12:03:23 +0200

    build: add support for Boost 1.40

--- CMakeModules/FindBoost.cmake
+++ CMakeModules/FindBoost.cmake
@@ -298,7 +298,7 @@ else(Boost_FIND_VERSION_EXACT)
   # The user has not requested an exact version.  Among known
   # versions, find those that are acceptable to the user request.
   set(_Boost_KNOWN_VERSIONS ${Boost_ADDITIONAL_VERSIONS}
-    "1.39.0" "1.39" "1.38.0" "1.38" "1.37.0" "1.37"
+    "1.40.0" "1.40" "1.39.0" "1.39" "1.38.0" "1.38" "1.37.0" "1.37"
     "1.36.1" "1.36.0" "1.36" "1.35.1" "1.35.0" "1.35" "1.34.1" "1.34.0"
     "1.34" "1.33.1" "1.33.0" "1.33")
   set(_boost_TEST_VERSIONS)
