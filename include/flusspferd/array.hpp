3d36e25d5dfd7f5815ef3f07a54b13321cd1f4fb
Rüdiger Sonderfeld <ruediger@c-plusplus.de>
2009-09-11 15:33:46 +0200

    started implementing an array iterator

--- include/flusspferd/array.hpp
+++ include/flusspferd/array.hpp
@@ -28,6 +28,8 @@ THE SOFTWARE.
 
 #include "object.hpp"
 #include <boost/utility/in_place_factory.hpp>
+#include <boost/iterator/iterator_facade.hpp>
+#include <cassert>
 
 namespace flusspferd {
 
@@ -63,6 +65,62 @@ public:
   /// Set an array element.
   void set_element(std::size_t n, value const &x);
 
+  class iterator
+    : public boost::iterator_facade<
+    iterator, value, boost::random_access_traversal_tag, value
+    >
+  {
+    typedef boost::iterator_facade<
+      iterator, value, boost::random_access_traversal_tag, value
+      > parent_t;
+
+    array const *a;
+    std::size_t pos;
+  public:
+    explicit
+    iterator(array const &a, std::size_t pos = 0) : a(&a),   pos(pos)  { }
+    iterator(iterator const &i)                   : a(i.a), pos(i.pos) { }
+    
+    iterator &operator=(iterator const &o) {
+      a = o.a;
+      pos = o.pos;
+      return *this;
+    }
+
+  private:
+    friend class boost::iterator_core_access;
+
+    void increment() {
+      assert(a);
+      ++pos;
+      assert(pos <= a->size());
+    }
+    void decrement() {
+      assert(a && pos > 0);
+      --pos;
+    }
+    void advance(std::size_t n) {
+      assert(a);
+      pos += n;
+      assert(pos <= a->size());
+    }
+    bool equal(iterator const &i) const {
+      assert(a && i.a);
+      return a == i.a && pos == i.pos;
+    }
+    parent_t::reference dereference() const {
+      assert(a && pos < a->size());
+      return a->get_element(pos);
+    }
+    parent_t::difference_type distance_to(iterator const &i) const {
+      assert(a && a == i.a);
+      typedef parent_t::difference_type diff_t;
+      return static_cast<diff_t>(pos) - static_cast<diff_t>(i.pos);
+    }
+  };
+
+  iterator begin() const { return iterator(*this); }
+  iterator end()   const { return iterator(*this, size()); }
 private:
   void check();
 };
