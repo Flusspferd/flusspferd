732fa39c6ed21f86ce345d8f55ea4c07b9cb15b6
Vinzenz 'evilissimo' Feenstra <contact@virtuosic-bytes.com>
2009-09-16 22:13:21 +0200

    Added iconv for windows

---
+++ libs/iconv/premake4.lua
@@ -0,0 +1,28 @@
+-- vim:ts=4:st=4:sts=4:et:
+
+project "iconv-static"
+    kind "StaticLib"
+    language "C"
+    location "."
+    targetdir(solution().targetdir)
+    targetname "iconv_static"
+
+    files { "src/win_iconv.c" }        
+
+    configuration "Release"
+        flags { "Optimize" }        
+
+project "iconv"
+    kind "SharedLib"
+    language "C"
+    location "."
+    targetdir(solution().targetdir)
+    targetname "iconv"
+
+    files { "src/win_iconv.c" }
+    defines {"HIPPOENV_SHARED_BUILD"}
+    flags { "Symbols" }
+
+    configuration "Release"
+        flags { "Optimize" }        
+
