c6d4f0df52aedd6a972a40be0600b6573868fcfe
Ash Berlin <ash_github@firemirror.com>
2009-09-26 23:32:59 +0100

    src/js, test/js: fix strict mode warnings.

--- test/js/test-http-headers.js
+++ test/js/test-http-headers.js
@@ -40,7 +40,7 @@ exports.test_simple = function() {
 exports.test_multiline = function() {
   const hdr_str = "Set-Cookie: value=foo\r\n   bar;path=/\r\n";
 
-  got = HTTP.Headers.parse(hdr_str);
+  var got = HTTP.Headers.parse(hdr_str);
   asserts.same(
     got,
     new HTTP.Headers({"Set-Cookie": "value=foo bar;path=/"}),
