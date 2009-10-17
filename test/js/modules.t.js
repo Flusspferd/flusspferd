2393627304cc098a9bff16aacf1e1a1687298dc2
Ash Berlin <ash_github@firemirror.com>
2009-10-17 14:32:10 +0100

    Use include style in tests to be relative (support to out of tree test runs)

--- test/js/modules.t.js
+++ test/js/modules.t.js
@@ -1,16 +1,14 @@
-require.paths.unshift('test/js/lib/modules-test');
-
 let test = require('test'),
     asserts = test.asserts;
 
 
 exports.test_RequireId = function () {
-  asserts.matches(require.id, "test/js/modules.t.js$");
+  asserts.matches(require.id, "file://.*/test/js/modules.t.js$");
 }
 
 exports.test_InstanceOf = function () {
-  var a1 = require('a1');
-  var a2 = require('a2');
+  var a1 = require('./lib/modules-test/a1');
+  var a2 = require('./lib/modules-test/a2');
   asserts.instanceOf(a1.array(), Array, "Array in a1 is instanceof main Array");
   asserts.instanceOf(a2.array(), Array, "Array in a2 is instanceof main Array");
   asserts.instanceOf([1,2,3], Array, "sanity check");
@@ -18,7 +16,7 @@ exports.test_InstanceOf = function () {
 
 var global = this;
 exports.test_varPolution = function() {
-  require('a2');
+  require('./lib/modules-test/a2');
   asserts.same("a2" in global, false, "no a2 variable");
 }
 
