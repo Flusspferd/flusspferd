35037250125e094bea3c4e3b5380d801ef781cf4
Aristid Breitkreuz <aristid.breitkreuz@gmx.de>
2009-10-26 20:14:15 +0100

    new_create: flusspferd::create<array>(...) works now

---
+++ test/test_create.cpp
@@ -0,0 +1,57 @@
+// vim:ts=2:sw=2:expandtab:autoindent:filetype=cpp:enc=utf-8:
+/*
+The MIT License
+
+Copyright (c) 2009 Flusspferd contributors (see "CONTRIBUTORS" or
+                                       http://flusspferd.org/contributors.txt)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+#include "flusspferd/create.hpp"
+#include "flusspferd/value_io.hpp"
+#include "test_environment.hpp"
+#include <boost/assign/list_of.hpp>
+
+using namespace flusspferd::param;
+using namespace boost::assign;
+
+BOOST_FIXTURE_TEST_SUITE( with_context, context_fixture )
+
+BOOST_AUTO_TEST_CASE( array ) {
+  flusspferd::array a(flusspferd::create<flusspferd::array>());
+  BOOST_CHECK_EQUAL(a.length(), 0);
+
+  a = flusspferd::create<flusspferd::array>(_length = 5);
+  BOOST_CHECK_EQUAL(a.length(), 5);
+
+  a = flusspferd::create<flusspferd::array>(3);
+  BOOST_CHECK_EQUAL(a.length(), 3);
+
+  a = flusspferd::create<flusspferd::array>(_contents = list_of(1)(2));
+  BOOST_CHECK_EQUAL(a.length(), 2);
+  BOOST_CHECK_EQUAL(a.get_element(0), flusspferd::value(1));
+  BOOST_CHECK_EQUAL(a.get_element(1), flusspferd::value(2));
+
+  a = flusspferd::create<flusspferd::array>(list_of(9));
+  BOOST_CHECK_EQUAL(a.length(), 1);
+  BOOST_CHECK_EQUAL(a.get_element(0), flusspferd::value(9));
+}
+
+BOOST_AUTO_TEST_SUITE_END()
