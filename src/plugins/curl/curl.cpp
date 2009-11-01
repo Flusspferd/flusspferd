cd898120faec70b406c131c317e6612382a5f2cb
Rüdiger Sonderfeld <ruediger@c-plusplus.de>
2009-11-01 03:39:30 +0100

    curl: continued implementing options. New internal API. Simple example working

--- src/plugins/curl/curl.cpp
+++ src/plugins/curl/curl.cpp
@@ -39,9 +39,9 @@ THE SOFTWARE.
 #include <boost/ptr_container/ptr_unordered_map.hpp>
 #include <boost/exception/get_error_info.hpp>
 #include <boost/assign/ptr_map_inserter.hpp>
-#include <boost/version.hpp>
+#include <boost/any.hpp>
 
-#include <iostream>
+//#include <iostream> //DEBUG
 
 using namespace flusspferd;
 
@@ -81,11 +81,10 @@ namespace {
 	namespace {
 		struct handle_option {
 			virtual ~handle_option() =0;
-			virtual value default_value() const { return value(); }
-			virtual property_attributes default_attributes() const {
-				return property_attributes();
-			}
-			virtual void handle(Easy &e, value const &v) const =0;
+			virtual function getter() const =0;
+			virtual function setter() const =0;
+			virtual boost::any data() const =0;
+			virtual CURLoption what() const =0;
 		};
 		handle_option::~handle_option() { }
 
@@ -101,6 +100,9 @@ namespace {
 	 (constructible, false)
 	 )
 	{
+	public: // TODO
+		typedef boost::unordered_map<CURLoption, boost::any> data_map_t;
+		data_map_t data;
 		Easy &parent;
 	public:
 		EasyOpt(flusspferd::object const &self, Easy &parent)
@@ -133,18 +135,29 @@ namespace {
     CURL *handle;
 		EasyOpt &opt;
 
+	public: // TODO
+		object writefunction_callback;
 		static size_t writefunction(void *ptr, size_t size, size_t nmemb, void *stream) {
+			//std::cout << "writefunction" << std::endl;
 			assert(stream);
 			Easy &self = *reinterpret_cast<Easy*>(stream);
-			byte_array data(object(),
-											reinterpret_cast<byte_array::element_type*>(ptr),
-											size*nmemb);
-			arguments arg;
-			arg.push_back(value(data));
-			arg.push_back(value(size));
-			object callback = self.opt.get_property("writeFunction").to_object();
-			value v = callback.call(arg);
-			return v.to_number();
+			if(self.writefunction_callback.is_null()) {
+				//std::cout << "writefunction*1" << std::endl;
+				return 0;
+			}
+			else {
+				//std::cout << "writefunction^2" << std::endl;
+				byte_array data(object(),
+												reinterpret_cast<byte_array::element_type*>(ptr),
+												size*nmemb);
+				arguments arg;
+				arg.push_back(value(data));
+				arg.push_back(value(size));
+				//std::cout << "writefunction^3" << std::endl;
+				value v = self.writefunction_callback.call(arg);
+				//std::cout << "writefunction^4" << std::endl;
+				return v.to_number();
+			}
 		}
 
 	protected:
@@ -187,32 +200,7 @@ namespace {
     }
     ~Easy() { cleanup(); }
 
-		/**
-		 * Applies all options set in EasyOpt.
-		 * This is called automatically.
-		 * Shouldn't be necessary to call it manually!
-		 *
-		 * Not exported.
-		 *
-		 * Maybe this should be called by unwrap and be private ...
-		 */
-		void apply_options() {
-			std::cout << "apply_options()\n";
-			options_map_t const &options = get_options();
-			for(property_iterator i = opt.begin();
-					i != opt.end();
-					++i)
-			{
-				std::cout << "\t" << i->to_std_string() << "\n";
-				options_map_t::const_iterator option = options.find(i->to_std_string());
-				if(option != options.end()) {
-					option->second->handle(*this, opt.get_property(*i));
-				}
-			}
-		}
-
     void perform() {
-			apply_options();
 			CURLcode res = curl_easy_perform(get());
 			if(res != 0) {
 				throw flusspferd::exception(std::string("curl_easy_perform: ") +
@@ -252,27 +240,10 @@ namespace {
 		//private:
 		template<typename T>
 		void do_setopt(CURLoption what, T data) {
-			std::cout << "setopt (" << (unsigned)what << ' ' << data << '\n'; // DEBUG
 			CURLcode res = curl_easy_setopt(get(), what, data);
 			if(res != 0) {
 				throw flusspferd::exception(std::string("curl_easy_setopt: ") +
 																		curl_easy_strerror(res));
-			}
-		}
-
-		void do_setopt_function(CURLoption what, bool default_) {
-			if(default_) {
-				do_setopt(what, 0x0);
-			}
-			else {
-				switch(what) {
-				case CURLOPT_WRITEFUNCTION:
-					do_setopt(CURLOPT_WRITEFUNCTION, &writefunction);
-					do_setopt(CURLOPT_WRITEDATA, this);
-					break;
-				default:
-					throw flusspferd::exception("unkown function option");
-				};
 			}
 		}
   };
@@ -284,47 +255,93 @@ namespace {
   }
 
 	namespace {
+		template<CURLoption What>
 		struct integer_option : handle_option {
-			integer_option(CURLoption what) : what(what) { }
-			value default_value() const { return value(0); }
-			void handle(Easy &e, value const &v) const {
-				e.do_setopt(what, v.to_number());
+			function getter() const {
+				return create_native_method(object(), "$get_", &get);
 			}
+			function setter() const {
+				return create_native_method(object(), "$set_", &set);
+			}
+			boost::any data() const { return 0l; }
+			CURLoption what() const { return What; }
   	private:
-			CURLoption what;
+			static long get(EasyOpt *o) {
+				assert(o);
+				return boost::any_cast<long>(o->data[What]);
+			}
+			static void set(EasyOpt *o, long opt) {
+				assert(o);
+				o->data[What] = opt;
+				o->parent.do_setopt(What, opt);
+			}
 		};
 
+		template<CURLoption What>
 		struct string_option : handle_option {
-			string_option(CURLoption what) : what(what) { }
-			value default_value() const { return value(""); }
-			void handle(Easy &e, value const &v) const {
-				e.do_setopt(what, v.to_std_string().c_str()); // TODO this won't work! string needs to be stored.
+			function getter() const {
+				return create_native_method(object(), "$get_", &get);
 			}
+			function setter() const {
+				return create_native_method(object(), "$set_", &set);
+			}
+			boost::any data() const { return std::string(); }
+			CURLoption what() const { return What; }
 		private:
-			CURLoption what;
+			static std::string get(EasyOpt *o) {
+				assert(o);
+				return boost::any_cast<std::string>(o->data[What]);
+			}
+			static void set(EasyOpt *o, std::string const &val) {
+				assert(o);
+				o->data[What] = val;
+				o->parent.do_setopt(What, boost::any_cast<std::string&>(o->data[What]).c_str());
+			}
 		};
 
-		struct function_option : handle_option {
-			function_option(CURLoption what) : what(what) { }
-			void handle(Easy &e, value const &v) const {
-				e.do_setopt_function(what, v.is_undefined_or_null());
+		struct write_function_option : handle_option {
+			static CURLoption const What = CURLOPT_WRITEFUNCTION;
+			function getter() const {
+				return create_native_method(object(), "$get_writeFunction", &get);
 			}
+			function setter() const {
+				return create_native_method(object(), "$set_writeFunction", &set);
+			}
+			boost::any data() const { return function(); }
+			CURLoption what() const { return What; }
 		private:
-			CURLoption what;
+			static object get(EasyOpt *o) {
+				assert(o);
+				return o->parent.writefunction_callback;
+			}
+			static void set(EasyOpt *o, object val) {
+				assert(o);
+				o->parent.writefunction_callback = val;
+				if(val.is_null()) {
+					o->parent.do_setopt(CURLOPT_WRITEFUNCTION, 0x0);
+				}
+				else {
+					o->parent.do_setopt(CURLOPT_WRITEFUNCTION, &Easy::writefunction);
+					o->parent.do_setopt(CURLOPT_WRITEDATA, &o->parent);
+				}
+			}
 		};
 
 		options_map_t const &get_options() {
 			static options_map_t map;
 			if(map.empty()) {
-				boost::assign::ptr_map_insert<integer_option>(map)
-					("verbose", CURLOPT_VERBOSE)
-					("header", CURLOPT_HEADER)
-					("noProgress", CURLOPT_NOPROGRESS)
-					("noSignal", CURLOPT_NOSIGNAL);
-				boost::assign::ptr_map_insert<function_option>(map)
-					("writeFunction", CURLOPT_WRITEFUNCTION);
-				boost::assign::ptr_map_insert<string_option>(map)
-					("url", CURLOPT_URL);
+				using namespace boost::assign;
+				ptr_map_insert< integer_option<CURLOPT_VERBOSE> >(map)
+					("verbose");
+				ptr_map_insert< integer_option<CURLOPT_HEADER> >(map)
+					("header");
+				ptr_map_insert< integer_option<CURLOPT_NOPROGRESS> >(map)
+					("noProgress");
+				ptr_map_insert< integer_option<CURLOPT_NOSIGNAL> >(map)
+					("noSignal");
+				ptr_map_insert< write_function_option >(map)
+					("writeFunction");
+				ptr_map_insert< string_option<CURLOPT_URL> >(map)("url");
 			}
 			return map;
 		}
@@ -334,8 +351,11 @@ namespace {
 		std::string const name = id.to_std_string();
 		options_map_t::const_iterator const i = get_options().find(name);
 		if(i != get_options().end()) {
-			define_property(id.get_string(), i->second->default_value(),
-											i->second->default_attributes());
+			property_attributes attr(no_property_flag,
+															 i->second->getter(),
+															 i->second->setter());
+			define_property(id.get_string(), value(), attr);
+			data[i->second->what()] = i->second->data();
 			return true;
 		}
 		else {
