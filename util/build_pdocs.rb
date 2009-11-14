9f74fd5a397d1c6a0279dc8a31050f7cb501b942
Ash Berlin <ash_github@firemirror.com>
2009-11-14 02:10:54 +0000

    Tidy up the pdoc runner script
    PDoc is now stored as a git module, and the code to give nice behaviour
    when its not there was shamelessly stolen from Prototype itself.
    
    Still to do: get the templates somewhere too.

---
+++ util/build_pdocs.rb
@@ -0,0 +1,74 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'rake'
+
+
+
+module HippoDocsHelper
+  ROOT_DIR      = File.join(File.expand_path(File.dirname(__FILE__)), '..')
+  DOC_DIR       = File.join(ROOT_DIR, 'build', 'html', 'js')
+
+
+  def self.require_pdoc
+    require_submodule('PDoc', 'pdoc')
+  end
+  
+
+  def self.build_docs()
+
+    rm_rf DOC_DIR
+    begin
+      files = Dir.glob( File.join(ROOT_DIR, "src", "**","*.pdoc") )
+      files << Dir.glob( File.join(ROOT_DIR, "src","**","*.js") )
+      files.flatten!
+
+      if files.empty?
+        puts "No input pdoc files!"
+        exit 1;
+      end
+      files << {
+        :output => DOC_DIR,
+        :templates => "vendor/pdoc-template/html",
+        :syntax_highlighter => :none,
+        #:index_page => 'README.markdown'
+      }
+      PDoc::Runner.new(*files).run
+      exit 0;
+    rescue PDoc::ParseError => e
+      puts e.message
+    rescue ArgumentError => e
+      puts "ArgError: " << e
+    end
+    exit 1;
+  end
+
+
+  def self.require_submodule(name, path)
+    begin
+      require path
+    rescue LoadError => e
+      missing_file = e.message.sub('no such file to load -- ', '')
+      if missing_file == path
+        puts "\nIt looks like you're missing #{name}. Just run:\n\n"
+        puts "  $ git submodule init"
+        puts "  $ git submodule update vendor/#{path}"
+        puts "\nand you should be all set.\n\n"
+      else
+        puts "\nIt looks like #{name} is missing the '#{missing_file}' gem. Just run:\n\n"
+        puts "  $ gem install #{missing_file}"
+        puts "\nand you should be all set.\n\n"
+      end
+      exit
+    end
+  end
+end
+
+%w[pdoc].each do |name|
+  $:.unshift File.join(HippoDocsHelper::ROOT_DIR, 'vendor', name, 'lib')
+end
+
+if __FILE__ == $0
+  HippoDocsHelper.require_pdoc
+  HippoDocsHelper.build_docs
+end
