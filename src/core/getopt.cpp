2ec5506fadafd58f0598031a04d402346a0fdf4b
Aristid Breitkreuz <aristid.breitkreuz@gmx.de>
2009-12-01 23:18:20 +0100

    core/getopt: fix #169

--- src/core/getopt.cpp
+++ src/core/getopt.cpp
@@ -68,6 +68,7 @@ namespace {
 struct optspec {
   struct item_type {
     enum argument_type { required = 1, optional = 2, none = 0 };
+    std::string canonical_name;
     argument_type argument;
     root_function callback;
 
@@ -99,6 +100,7 @@ struct optspec {
         break;
 
       item_pointer data(new item_type);
+      data->canonical_name = name;
 
       options.insert(map_type::value_type(name, data));
 
@@ -150,6 +152,8 @@ struct optspec {
     if (!data)
       throw exception(("Unknown option " + name).c_str());
 
+    name = data->canonical_name;
+
     if (!result.has_property(name))
       result.set_property(name, flusspferd::create<array>());
     array arr(result.get_property_object(name));
@@ -184,6 +188,8 @@ struct optspec {
       item_pointer data = options[name];
       if (!data)
         throw exception(("Unknown option " + name).c_str());
+
+      name = data->canonical_name;
 
       if (!result.has_property(name))
         result.set_property(name, flusspferd::create<array>());
