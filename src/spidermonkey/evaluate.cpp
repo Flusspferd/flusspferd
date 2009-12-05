44d4644ff20592de1702f536a24d56e1768572ca
Aristid Breitkreuz <aristid.breitkreuz@gmx.de>
2009-12-05 20:13:55 +0100

    bug #159: apply Wes' suggestions

--- src/spidermonkey/evaluate.cpp
+++ src/spidermonkey/evaluate.cpp
@@ -91,7 +91,7 @@ value flusspferd::execute(char const *filename, object const &scope_) {
     scope = Impl::get_object(flusspferd::global());
 
   int oldopts = JS_GetOptions(cx);
-  JS_SetOptions(cx, oldopts | JSOPTION_COMPILE_N_GO );
+  JS_SetOptions(cx, oldopts | JSOPTION_COMPILE_N_GO);
   JSScript *script = JS_CompileUCScript(
     cx, scope,
     module_text.data(), module_text.length(),
@@ -106,19 +106,18 @@ value flusspferd::execute(char const *filename, object const &scope_) {
 
   JS_SetOptions(cx, oldopts);
 
-  value result;
+  // JS_NewScriptObject is needed because otherwise the Garbage Collector
+  // may go amok!
+  root_object script_o(Impl::wrap_object(JS_NewScriptObject(cx, script)));
+
+  root_value result;
 
   JSBool ok = JS_ExecuteScript(cx, scope, script, Impl::get_jsvalp(result));
 
-  if (!ok) {
-    exception e("Script execution failed");
-    if (!e.is_js_exception()) {
-      JS_DestroyScript(cx, script);
-      throw e;
-    }
-  }
+  if (!ok)
+    throw exception("Script execution failed");
 
-  JS_DestroyScript(cx, script);
+  JS_MaybeGC(cx);
 
   return result;
 }
