82258c7ae6985deb1b3f8acd17605ade8a23676a
Ash Berlin <ash_github@firemirror.com>
2009-12-06 16:51:07 +0000

    Make test_regression_159 compile on OSX

--- test/test_regression_159.cpp
+++ test/test_regression_159.cpp
@@ -23,7 +23,10 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
-#include <flusspferd.hpp>
+
+#include <flusspferd/create_on.hpp>
+#include <flusspferd/evaluate.hpp>
+
 #include <iostream>
 #include "test_environment.hpp"
 
