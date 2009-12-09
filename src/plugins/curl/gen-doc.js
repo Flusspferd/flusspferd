ec7cd5b1c5398c973c5802f5e80447cd15bd203b
Rüdiger Sonderfeld <ruediger@c-plusplus.de>
2009-12-09 12:43:22 +0100

    curl: updated doc generation

--- src/plugins/curl/gen-doc.js
+++ src/plugins/curl/gen-doc.js
@@ -6,28 +6,51 @@ function print(x) {
   require('system').stdout.print(x);
 }
 
+function trim(x) {
+  return x.replace(/\s*$/,'').replace(/^\s*/,'');
+}
+
 const file = 'curl.cpp';
 
 var fh = new io.File();
 fh.open(file);
 
 var in_doc = false;
+var in_header = false;
+var header = new Array;
 var line;
 while( (line = fh.readLine()) ) {
   if(line.indexOf('BEGIN DOC{') != -1) {
+    print('\n/**');
     in_doc = true;
   }
   else if(line.indexOf('}END DOC') != -1) {
+    print(' **/');
     in_doc = false;
+  }
+  else if(in_header) {
+    if(line.match(/}HEADER/)) {
+      for each(var i in header) {
+        print(' * ' + i);
+      }
+      print(' * ');
+      in_header = false;
+    }
+    header.push(trim(line));
   }
   else if(in_doc) {
     var m;
     if( (m = line.match(/ptr_map_insert< (.*?)_option<(.*?)> >\(map\)\("(.*?)"\)/)) ) {
-      var out = ' * ' + m[2] + ' -> ' + m[3] + ' (' + m[1]+ ')';
+      var out = ' * + [' + m[2] +
+                  '](http://curl.haxx.se/libcurl/c/curl_easy_setopt.html#' +
+                    m[2].replace(/_/g,'') + ') -> ' + m[3] + ' (' + m[1]+ ')';
       if( (m = line.match(/\/\/ (.*)/)) ) {
         out += ' ' + m[1];
       }
       print(out);
+    }
+    else if( (m = line.match(/HEADER{/))) {
+      in_header = true;
     }
     else if( (m = line.match(/^\s*\/\/ (.*)/))) {
       if(m[1].indexOf('TODO') == -1) {
