2569162dddbceeff9cc7c98dbda212b138f0d7cf
Rüdiger Sonderfeld <ruediger@c-plusplus.de>
2009-12-10 05:38:05 +0100

    curl: updated docs (added implementation notes, fixed bug in docs.sh and updated TODO)

--- util/docs.sh
+++ util/docs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 doxygen help/Doxyfile
 # this is a hack. find sth. better
-if [ -f ./src/plugins/curl/gen-doc.js -a -f ./src/plugins/curl/curl.cpp]; then
+if [ -f ./src/plugins/curl/gen-doc.js -a -f ./src/plugins/curl/curl.cpp ]; then
     ./util/jsrepl.sh ./src/plugins/curl/gen-doc.js ./src/plugins/curl/curl.cpp > ./src/plugins/curl/options.pdoc
 fi
 ./util/build_pdocs.rb
