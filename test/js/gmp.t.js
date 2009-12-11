ea74c5ee41129fa3b906a13992f2b78b58d2cf8c
Rüdiger Sonderfeld <ruediger@c-plusplus.de>
2009-12-11 19:56:42 +0100

    gmp: wrote unit test for gmp.Integer

---
+++ test/js/gmp.t.js
@@ -0,0 +1,32 @@
+const gmp = require('gmp');
+const asserts = require('test').asserts;
+
+exports.testInteger = function() {
+  var j = gmp.Integer(10);
+  asserts.same(j.toInt(), 10);
+  asserts.same(j.toString(), '10');
+  asserts.same(j.toString(2), '1010');
+  var z = gmp.Integer(1);
+  asserts.same(z.toInt(), 1);
+  asserts.same(z.toString(), '1');
+  asserts.same(z.toString(2), '1');
+  var r = j.add(z);
+  asserts.same(r.toInt(), 11);
+  asserts.same(r.toString(), '11');
+  asserts.same(r.toString(2), '1011');
+  z = gmp.Integer(-5);
+  asserts.same(z.toInt(), -5);
+  asserts.same(z.toString(), '-5');
+  asserts.same(z.toString(2), '-101');
+  r = j.add(z);
+  asserts.same(r.toInt(), 5);
+  asserts.same(r.toString(), '5');
+  asserts.same(r.toString(2), '101');
+  r = z.abs();
+  asserts.same(r.toInt(), 5);
+  asserts.same(r.toString(), '5');
+  asserts.same(r.toString(2), '101');
+};
+
+if (require.main === module)
+  require('test').runner(exports);
