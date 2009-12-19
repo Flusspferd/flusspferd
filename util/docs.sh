b240b780897f0afcf3fcd365d253f4b6cde569b5
Aristid Breitkreuz <aristid.breitkreuz@gmx.de>
2009-12-19 02:10:32 +0100

    curl/doc: fetch options from different file

--- util/docs.sh
+++ util/docs.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 doxygen help/Doxyfile
+
 # this is a hack. find sth. better
-if [ -f ./src/plugins/curl/gen-doc.js -a -f ./src/plugins/curl/curl.cpp ]; then
-    ./util/jsrepl.sh ./src/plugins/curl/gen-doc.js ./src/plugins/curl/curl.cpp > ./src/plugins/curl/options.pdoc
-fi
+./util/jsrepl.sh \
+    ./src/plugins/curl/gen-doc.js \
+    ./src/plugins/curl/get_options.cpp \
+    > ./src/plugins/curl/options.pdoc
+
 ./util/build_pdocs.rb
 rm -f ./src/plugins/curl/options.pdoc
+
 groff -man -Thtml build/flusspferd.1 > build/html/flusspferd.1.html
