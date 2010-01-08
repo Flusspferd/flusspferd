6597e1647c6432150057b90f076c995e581f8b4e
Rüdiger Sonderfeld <ruediger@c-plusplus.de>
2010-01-08 20:45:49 +0100

    subprocess: implemented communicate and more options for popen

--- libflusspferd/subprocess.cpp
+++ libflusspferd/subprocess.cpp
@@ -37,6 +37,7 @@ void flusspferd::load_subprocess_module(object &ctx) {
 //#include "flusspferd/io/stream.hpp"
 #include "flusspferd/array.hpp"
 #include "flusspferd/create.hpp"
+#include "flusspferd/create/object.hpp"
 #include "flusspferd/create/function.hpp"
 #include "flusspferd/class_description.hpp"
 #include "flusspferd/property_iterator.hpp"
@@ -46,9 +47,20 @@ void flusspferd::load_subprocess_module(object &ctx) {
 #include <signal.h>
 #include <unistd.h>
 #include <errno.h>
+#include <fcntl.h>
 
+#ifdef HAVE_EPOLL
+#include <sys/epoll.h>
+#else
+#include <sys/select.h>
+#include <sys/time.h>
+#endif
+
+#include <map>
 #include <vector>
 #include <cstdio>
+#include <iterator>
+#include <algorithm>
 
 #include "flusspferd/value_io.hpp" // DEBUG
 #include <iostream> // DEBUG
@@ -61,6 +73,166 @@ extern char** environ;
 # endif
 
 using namespace flusspferd;
+
+namespace {
+  void setnonblock(int fd) {
+    int flags = fcntl(fd, F_GETFL, 0);
+    if(flags == -1) {
+      int const errno_ = errno;
+      throw flusspferd::exception(std::string("fcntl: ") + std::strerror(errno_));
+    }
+    flags |= O_NONBLOCK;
+    if(fcntl(fd, F_SETFL, flags) == -1) {
+      int const errno_ = errno;
+      throw flusspferd::exception(std::string("fcntl: ") + std::strerror(errno_));
+    }
+  }
+
+  class fdpoll {
+#ifdef HAVE_EPOLL
+    int epollfd;
+#else // select
+    std::map<int, unsigned> fds;
+#endif
+  public:
+    fdpoll();
+    ~fdpoll();
+
+    enum { read = 1, write = 2, error = 4 };
+    void add(int fd, unsigned what);
+    void remove(int fd);
+    static int const indefinitely = -1;
+    std::vector< std::pair<int, unsigned> > wait(int timeout_ms = indefinitely);
+  };
+
+  bool flagset(unsigned f, unsigned in) {
+    return (in & f) == f;
+  }
+
+#ifdef HAVE_EPOLL
+  fdpoll::fdpoll() {
+    epollfd = ::epoll_create1(FD_CLOEXEC);
+    if(epollfd == -1) {
+      int const errno_ = errno;
+      throw flusspferd::exception(std::string("epoll_create: ") + std::strerror(errno_));
+    }
+  }
+  fdpoll::~fdpoll() {
+    close(epollfd);
+  }
+
+  void fdpoll::add(int fd, unsigned what) {
+    epoll_event ev;
+    std::memset(&ev, 0, sizeof(ev));
+    if(flagset(fdpoll::read, what)) {
+      ev.events |= EPOLLIN;
+    }
+    if(flagset(fdpoll::write, what)) {
+      ev.events |= EPOLLOUT;
+    }
+    // always waits for EPOLLERR so no need to check/set
+    ev.data.fd = fd;
+    if(epoll_ctl(epollfd, EPOLL_CTL_ADD, fd, &ev) == -1) {
+      int const errno_ = errno;
+      throw flusspferd::exception(std::string("epoll_ctl: ") + std::strerror(errno_));
+    }
+  }
+
+  void fdpoll::remove(int fd) {
+    epoll_event ev;
+    if(epoll_ctl(epollfd, EPOLL_CTL_DEL, fd, &ev) == -1) {
+      int const errno_ = errno;
+      throw flusspferd::exception(std::string("epoll_ctl: ") + std::strerror(errno_));
+    }
+  }
+
+  std::vector< std::pair<int, unsigned> > fdpoll::wait(int timeout_ms) {
+    enum { EVENTS_N = 8 };
+    epoll_event events[EVENTS_N];
+    int nfds = epoll_wait(epollfd, events, EVENTS_N, timeout_ms);
+    if(nfds == -1) {
+      int const errno_ = errno;
+      throw flusspferd::exception(std::string("epoll_wait: ") + std::strerror(errno_));
+    }
+    std::vector< std::pair<int, unsigned> > ret;
+    ret.reserve(nfds);
+    for(int i = 0; i < nfds; ++i) {
+      unsigned what = 0;
+      if(flagset(EPOLLIN, events[i].events)) {
+        what |= fdpoll::read;
+      }
+      if(flagset(EPOLLOUT, events[i].events)) {
+        what |= fdpoll::write;
+      }
+      if(flagset(EPOLLERR, events[i].events)) {
+        what |= fdpoll::error;
+      }
+      ret.push_back( std::make_pair(events[i].fd, what) );
+    }
+    return ret;
+  }
+#else // select
+  fdpoll::fdpoll() { }
+  fdpoll::~fdpoll() { }
+  void fdpoll::add(int fd, unsigned what) {
+    fds[fd] = what;
+  }
+  void fdpoll::remove(int fd) {
+    fds.erase(fd);
+  }
+  std::vector< std::pair<int, unsigned> > fdpoll::wait(int timeout_ms) {
+    fd_set rfds, wfds, efds;
+    int nfds;
+    FD_ZERO(&rfds);
+    FD_ZERO(&wfds);
+    FD_ZERO(&efds);
+    // TODO this is slow. maybe add a fdpoll::setup
+    for(std::map<int, unsigned>::const_iterator i = fds.begin(); i != fds.end(); ++i) {
+      nfds = std::max(i->first, nfds);
+      if(flagset(fdpoll::read, i->second)) {
+        FD_SET(i->first, &rfds);
+      }
+      if(flagset(fdpoll::write, i->second)) {
+        FD_SET(i->first, &wfds);
+      }
+      if(flagset(fdpoll::error, i->second)) {
+        FD_SET(i->first, &efds);
+      }
+    }
+    timeval tv;
+    if(timeout_ms != fdpoll::indefinitely) {
+      tv.tv_sec = timeout_ms / 1000;
+      tv.tv_usec = (timeout_ms % 1000) * 1000;
+    }
+    int retfds = select(nfds+1, &rfds, &wfds, &efds,
+                        timeout_ms != fdpoll::indefinitely ? &tv : 0x0);
+    if(retfds == -1) {
+      int const errno_ = errno;
+      throw flusspferd::exception(std::string("epoll_ctl: ") + std::strerror(errno_));
+    }
+    else if(retfds == 0) { // timeout
+      return std::vector< std::pair<int, unsigned> >();
+    }
+
+    std::vector< std::pair<int, unsigned> > ret;
+    ret.reserve(retfds);
+    for(std::map<int, unsigned>::const_iterator i = fds.begin(); i != fds.end(); ++i) {
+      unsigned what = 0;
+      if(flagset(fdpoll::read, i->second) && FD_ISSET(i->first, &rfds)) {
+        what |= fdpoll::read;
+      }
+      if(flagset(fdpoll::write, i->second) && FD_ISSET(i->first, &wfds)) {
+        what |= fdpoll::write;
+      }
+      if(flagset(fdpoll::error, i->second) && FD_ISSET(i->first, &efds)) {
+        what |= fdpoll::error;
+      }
+      ret.push_back(std::make_pair(i->first, what));
+    }
+    return ret;
+  }
+#endif
+}
 
 FLUSSPFERD_CLASS_DESCRIPTION(
   subprocess,
@@ -84,6 +256,10 @@ FLUSSPFERD_CLASS_DESCRIPTION(
   pid_t pid;
   bool finished;
   int returncode;
+
+  int stdinfd;
+  int stdoutfd;
+  int stderrfd;
 
   value wait_impl(bool poll = false) {
     if(finished) {
@@ -114,9 +290,28 @@ FLUSSPFERD_CLASS_DESCRIPTION(
     }
     return value(object());
   }
+  bool read_impl(int fd, std::string &buffer, fdpoll &poll) { // return true if done
+    enum { BUFSIZE = 2048 };
+    char buf[BUFSIZE];
+    ssize_t n = ::read(stdoutfd, buf, BUFSIZE);
+    if(n < 0) {
+      int const errno_ = errno;
+      if(errno_ == EAGAIN || errno_ == EWOULDBLOCK) {
+        return false;
+      }
+      throw flusspferd::exception(std::string("write: ") + std::strerror(errno_));
+    }
+    else if(n == 0) { // eof
+      poll.remove(fd);
+      return true; // done
+    }
+    buffer += buf;
+    return false;
+  }
  public:
-  subprocess(object const &self, pid_t pid)
-    : base_type(self), pid(pid), finished(false)
+  subprocess(object const &self, pid_t pid, int stdinfd, int stdoutfd, int stderrfd)
+    : base_type(self), pid(pid), finished(false), stdinfd(stdinfd), stdoutfd(stdoutfd),
+      stderrfd(stderrfd)
   { }
 
   value poll() {
@@ -126,8 +321,77 @@ FLUSSPFERD_CLASS_DESCRIPTION(
     return wait_impl(false);
   }
   object communicate(boost::optional<std::string> const &in) {
-    (void)in;
-    return object();
+    if(in && stdinfd == -1) {
+      throw flusspferd::exception("subprocess:communicate: No stdin pipe open but got input");
+    }
+
+    bool done_stdin = stdinfd == -1 || !in;
+    bool done_stdout = stdoutfd == -1;
+    bool done_stderr = stderrfd == -1;
+
+    fdpoll poll;
+    if(!done_stdin) {
+      setnonblock(stdinfd);
+      poll.add(stdinfd, fdpoll::write);
+    }
+    if(!done_stdout) {
+      setnonblock(stdoutfd);
+      poll.add(stdoutfd, fdpoll::read);
+    }
+    if(!done_stderr) {
+      setnonblock(stderrfd);
+      poll.add(stderrfd, fdpoll::read);
+    }
+
+    std::size_t bufpos = 0; // what part of `in` to write next
+
+    std::string stdoutbuf;
+    std::string stderrbuf;
+
+    while( !(done_stdin || done_stdout || done_stderr) ) {
+      std::vector< std::pair<int, unsigned> > ret = poll.wait(fdpoll::indefinitely);
+      for(std::vector< std::pair<int, unsigned> >::const_iterator i = ret.begin();
+          i != ret.end();
+          ++i)
+      {
+        if(!done_stdin && i->first == stdinfd && flagset(i->second, fdpoll::write)) {
+          ssize_t n = ::write(stdinfd, &(*in)[0] + bufpos, in->size() - bufpos);
+          if(n < 0) {
+            int const errno_ = errno;
+            if(errno_ == EAGAIN || errno_ == EWOULDBLOCK) {
+              continue;
+            }
+            else if(errno_ == EPIPE) {
+              poll.remove(stdinfd);
+              stdinfd = -1;
+              done_stdin = true;
+              continue;
+            }
+            throw flusspferd::exception(std::string("write: ") + std::strerror(errno_));
+          }
+          bufpos += std::size_t(n);
+          if(bufpos >= in->size()) {
+            poll.remove(stdinfd);
+            done_stdin = true;
+          }
+        }
+        else if(!done_stdout && i->first == stdoutfd && flagset(i->second, fdpoll::read)) {
+          done_stdout = read_impl(stdoutfd, stdoutbuf, poll);
+        }
+        else if(!done_stderr && i->first == stderrfd && flagset(i->second, fdpoll::read)) {
+          done_stderr = read_impl(stderrfd, stderrbuf, poll);
+        }
+      }
+    }
+
+    value rc = wait();
+
+    object ret = flusspferd::create<flusspferd::object>();
+    ret.define_property("returncode", rc);
+    ret.define_property("stdout", value(stdoutbuf));
+    ret.define_property("stderr", value(stderrbuf));
+
+    return ret;
   }
   void send_signal(int sig) {
     if(pid != 0) {
@@ -156,8 +420,8 @@ FLUSSPFERD_CLASS_DESCRIPTION(
   object get_stderr() { return object(); }
   object get_stdout() { return object(); }
 
-  static subprocess &create(pid_t p) {
-    return flusspferd::create<subprocess>(boost::fusion::vector1<pid_t>(p));
+  static subprocess &create(pid_t p, int in, int out, int err) {
+    return flusspferd::create<subprocess>(boost::fusion::make_vector(p, in, out, err));
   }
 };
 
@@ -233,8 +497,53 @@ namespace {
       _exit(127);
     }
 
-    // ...
-    return subprocess::create(pid);
+    if(stdin_) {
+      close(stdinp[0]);
+    }
+    else {
+      stdinp[1] = -1;
+    }
+
+    if(stdout_) {
+      close(stdoutp[1]);
+    }
+    else {
+      stdoutp[0] = -1;
+    }
+
+    if(stderr_) {
+      close(stderrp[1]);
+    }
+    else {
+      stderrp[0] = -1;
+    }
+    return subprocess::create(pid, stdinp[1], stdoutp[0], stderrp[0]);
+  }
+
+  std::vector<char const*> array2args(object o) {
+    assert(o.is_array());
+    array a(o);
+    std::vector<char const*> args;
+    array::iterator const end = a.end();
+    for(array::iterator i = a.begin(); i != end; ++i) {
+      if(!i->is_string()) {
+        throw flusspferd::exception("popen array elements must be strings",
+                                    "TypeError");
+      }
+      args.push_back(i->get_string().c_str());
+    }
+    args.push_back(0x0);
+    return args;
+  }
+
+  std::vector<char const*> shellargs(value v) {
+    assert(v.is_string());
+    std::vector<char const*> args;
+    args.push_back("sh");
+    args.push_back("-c");
+    args.push_back(v.get_string().c_str());
+    args.push_back(0x0);
+    return args;
   }
 }
 
@@ -247,6 +556,9 @@ popen("foo", "w"); // popen(3)
 
 popen(["foo", "arg0", "arg1"], "r"); // similar to popen(3) but uses execve instead of shell
 
+popen("foo"); // runs cmd in shell and opens stdin/stdout/stderr
+popen([...]);
+
 popen(obj);
 obj = {
   args : Array containing the Arguments (args[0] is used as executable if .executable is null!,
@@ -258,23 +570,77 @@ obj = {
   stdout : optBoolean
   stderr : optBoolean
 };
+if shell is true then args[0] ist passed to the shell and the rest as arguments to the shell(!)
 
  */
 
 void Popen(flusspferd::call_context &x) {
   if(x.arg.size() == 1) {
-    if(!x.arg.front().is_object()) {
-      throw flusspferd::exception("popen: expects object as parameter", "TypeError");
+    if(x.arg.front().is_object()) {
+      if(x.arg.front().get_object().is_array()) {
+        std::vector<char const*> args = array2args(x.arg[0].get_object());       
+        std::vector<char const*> env;
+        x.result = do_popen(0x0, args, env, true, true, true);
+        return;
+      }
+      else {
+        object o = x.arg[0].get_object();
+        // args
+        if(!o.has_property("args") ||
+           !o.get_property("args").is_object() ||
+           !o.get_property("args").get_object().is_array())
+        {
+          throw flusspferd::exception("popen expectes Array args property", "TypeError");
+        }
+        std::vector<char const*> args = array2args(o.get_property("args").get_object());
+
+        // executable
+        char const *cmd = 0x0;
+        if(o.has_property("executable") && o.get_property("executable").is_string()) {
+          cmd = o.get_property("executable").get_string().c_str();
+        }
+
+        // shell TODO
+        bool shell = false;
+
+        // cwd TODO
+        char const *cwd = 0x0;
+
+        // stdin, stderr, stdout TODO
+        bool stdin_ = true;
+        bool stderr_ = true;
+        bool stdout_ = true;
+
+        // env TODO
+        std::vector<char const*> env;
+
+        if(shell) {
+          std::vector<char const*> shellargs;
+          shellargs.reserve(2 + args.size());
+          shellargs.push_back("sh");
+          shellargs.push_back("-c");
+          std::copy(args.begin(), args.end(), std::back_inserter(shellargs));
+          x.result = do_popen(0x0, shellargs, env, stdin_, stderr_, stdout_, cwd);
+        }
+        else {
+          x.result = do_popen(cmd, args, env, stdin_, stderr_, stdout_, cwd);
+        }
+        return;
+      }
+    }
+    else if(x.arg.front().is_string()) {
+      std::vector<char const*> args = shellargs(x.arg.front());
+      std::vector<char const*> env;
+      x.result = do_popen(0x0, args, env, true, true, true);
+      return;
+    }
+    else {
+      throw flusspferd::exception("popen: expects object or string as parameter", "TypeError");
     }
   }
   else if(x.arg.size() == 2) {
     if(x.arg[0].is_string() && x.arg[1].is_string()) {
-      std::vector<char const*> args;
-      args.push_back("sh");
-      args.push_back("-c");
-      args.push_back(x.arg[0].get_string().c_str());
-      args.push_back(0x0);
-
+      std::vector<char const*> args = shellargs(x.arg[0]);
       std::vector<char const*> env;
 
       bool in = false;
@@ -285,22 +651,11 @@ void Popen(flusspferd::call_context &x) {
         throw flusspferd::exception("popen: second parameter should be \"r\" or \"w\"");
       }
 
-      x.result = do_popen("/bin/sh", args, env, in, !in, false);
+      x.result = do_popen(0x0, args, env, in, !in, false);
       return;
     }
     else if(x.arg[0].is_object() && x.arg[0].get_object().is_array() && x.arg[1].is_string()) {
-      array a(x.arg[0].get_object());
-      std::vector<char const*> args;
-      array::iterator const end = a.end();
-      for(array::iterator i = a.begin(); i != end; ++i) {
-        if(!i->is_string()) {
-          throw flusspferd::exception("popen first arg: array elements must be strings",
-                                      "TypeError");
-        }
-        args.push_back(i->get_string().c_str());
-      }
-      args.push_back(0x0);
-
+      std::vector<char const*> args = array2args(x.arg[0].get_object());
       std::vector<char const*> env;
 
       bool in = false;
@@ -310,6 +665,7 @@ void Popen(flusspferd::call_context &x) {
       else if(!(x.arg[1].get_string() == "r")) {
         throw flusspferd::exception("popen: second parameter should be \"r\" or \"w\"");
       }
+
       x.result = do_popen(0x0, args, env, in, !in, false);
       return;
     }
