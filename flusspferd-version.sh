e7af65abd8f6357dcadc77ffdcc484a548841e89
Ash Berlin <ash_github@firemirror.com>
2010-01-30 21:22:18 +0000

    Move reading of version file into CMakeLists.txt so it is uses on win32.

--- flusspferd-version.sh
+++ flusspferd-version.sh
@@ -29,12 +29,8 @@ DEF_VER=dev
 LF='
 '
 
-# First see if there is a version file (included in release tarballs),
-# then try git-describe, then default.
-if test -f version
-then
-	VN=`cat version` || VN="$DEF_VER"
-elif test -d .git -o -f .git &&
+# Try git-describe, then default.
+if test -d .git -o -f .git &&
 	VN=`git describe --abbrev=4 HEAD 2>/dev/null` &&
 	case "$VN" in
 	*$LF*) (exit 1) ;;
