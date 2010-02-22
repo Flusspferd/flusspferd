8cd7687b68569bb4b4d58c0ba5c44c67dfa07083
Rüdiger Sonderfeld <ruediger@c-plusplus.de>
2010-02-22 23:09:34 +0100

    build: use "" in case someone uses strange dirnames

--- util/docs.sh
+++ util/docs.sh
@@ -17,14 +17,14 @@ if [ "$3" ]; then
 fi
 
 # this is yet another hack to set the builddir for doxygen
-cp help/Doxyfile $builddir/Doxyfile && echo "OUTPUT_DIRECTORY       = $builddir" >> $builddir/Doxyfile && $doxygen $builddir/Doxyfile
+cp help/Doxyfile "$builddir/Doxyfile" && echo "OUTPUT_DIRECTORY       = $builddir" >> "$builddir/Doxyfile" && "$doxygen" "$builddir/Doxyfile"
 
 # this is a hack. find sth. better
-$builddir/bin/flusspferd -c /dev/null \
-    $sourcedir/plugins/curl/gen-doc.js \
-    $sourcedir/plugins/curl/get_options.cpp \
-    > $builddir/options.pdoc
+"$builddir/bin/flusspferd" -c /dev/null \
+    "$sourcedir/plugins/curl/gen-doc.js" \
+    "$sourcedir/plugins/curl/get_options.cpp" \
+    > "$builddir/options.pdoc"
 
-$sourcedir/util/build_pdocs.rb $builddir/options.pdoc
+"$sourcedir/util/build_pdocs.rb" "$builddir/options.pdoc"
 
-groff -man -Thtml $builddir/flusspferd.1 > $builddir/html/flusspferd.1.html
+groff -man -Thtml "$builddir/flusspferd.1" > "$builddir/html/flusspferd.1.html"
